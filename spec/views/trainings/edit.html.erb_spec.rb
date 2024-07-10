3b4fbbd006db254a760b5bccda85e3b136a1e79c
David Lafontant <lafontant.david@gmail.com>
2024-07-10 18:18:50 -0400

    Scaffold training

---
+++ spec/views/trainings/edit.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "trainings/edit", type: :view do
+  let(:training) {
+    Training.create!(
+      name: "MyString",
+      description: "MyText",
+      credit: 1
+    )
+  }
+
+  before(:each) do
+    assign(:training, training)
+  end
+
+  it "renders the edit training form" do
+    render
+
+    assert_select "form[action=?][method=?]", training_path(training), "post" do
+
+      assert_select "input[name=?]", "training[name]"
+
+      assert_select "textarea[name=?]", "training[description]"
+
+      assert_select "input[name=?]", "training[credit]"
+    end
+  end
+end
