fe997a5e863f2c819268fbd9943753cde4170e71
David Lafontant <lafontant.david@gmail.com>
2024-07-10 19:13:15 -0400

    Create the validation for the course model and the association with User

---
+++ app/models/course.rb
@@ -0,0 +1,8 @@
+class Course < ApplicationRecord
+  belongs_to :teacher, class_name: 'User'
+  validates :name, :description, :credit, :code, presence: true
+  validates :name, length: {in: 8..40}, uniqueness: true
+  validates :code, length: {in: 6..8}, uniqueness: true
+  validates :description, length: {in: 30..300}
+  validates :credit, numericality: { only_integer: true , greater_than_or_equal_to: 0 }
+end
