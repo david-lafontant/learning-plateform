64cb7d8f5e34c6a4ef4d7e04086d76ade1d46ecb
David Lafontant <lafontant.david@gmail.com>
2024-07-11 20:35:24 -0400

    Destroy course_training records on course deleting

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,11 @@
 class Course < ApplicationRecord
   belongs_to :teacher, class_name: 'User'
-  has_many :course_trainings
-  has_many :trainings, through: course_trainings
+  has_many :course_trainings, dependent: :destroy
+  has_many :trainings, through: :course_trainings
   validates :name, :description, :credit, :code, presence: true
   validates :name, length: {in: 8..40}, uniqueness: true
   validates :code, length: {in: 6..8}, uniqueness: true
   validates :description, length: {in: 30..300}
   validates :credit, numericality: { only_integer: true , greater_than_or_equal_to: 0 }
+
 end
