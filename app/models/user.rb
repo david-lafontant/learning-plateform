383378c584458884f752668c7e7e4869e0ee7b25
David Lafontant <lafontant.david@gmail.com>
2024-07-12 17:12:31 -0400

    Setup many to many association between course and user through course_enrollement

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :courses, foreign_key: :teacher_id
+  has_many :course_enrollements, foreign_key: :student_id, dependent: :destroy
+  has_many :courses, through: :course_enrollements
   has_many :grades, foreign_key: :student_id
 end
