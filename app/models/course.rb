fa2f85d8aa668ef0e1a55d678944fea51b7a2a41
David Lafontant <lafontant.david@gmail.com>
2024-07-11 20:52:58 -0400

    Setup a one to many association relathionship between course and course_module

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,7 @@ class Course < ApplicationRecord
   belongs_to :teacher, class_name: 'User'
   has_many :course_trainings, dependent: :destroy
   has_many :trainings, through: :course_trainings
+  has_many : course_modules, dependent: :destroy
   validates :name, :description, :credit, :code, presence: true
   validates :name, length: {in: 8..40}, uniqueness: true
   validates :code, length: {in: 6..8}, uniqueness: true
