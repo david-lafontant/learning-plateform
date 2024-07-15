69f892e64a63addf3d26265e0f9946207da52e32
David Lafontant <lafontant.david@gmail.com>
2024-07-15 15:57:12 -0400

    Add prefix to distinguish between User and devise User routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
   resources :course_modules
   resources :courses
   resources :trainings
-  devise_for :users
+
+  devise_for :users, :path_prefix => 'system'
+
+
+
   root'page#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :course_enrollements#, only: [:show, :update]
 end
