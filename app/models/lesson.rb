92f8e8f5bf6c8d85410118e6bc6e334274509183
David Lafontant <lafontant.david@gmail.com>
2024-07-16 18:45:25 -0400

    Test and validate lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,9 @@
 class Lesson < ApplicationRecord
   belongs_to :course_module
   has_many_attached :materials
+
+  validates :title, presence: true, length: { in: 8..40 }
+  validates :content, presence: true, length: { in: 30..300 }
+  validates :course_module_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject do
+    described_class.new(
+      title: "lesson 4",
+      content: %q(
+        dctjhkydytcyghkjluydtcxgjnfrzx\gb_njm.lkjhgchnjmjnhbyfWSRFRDfghnjgfcv123456788765rfghygtffrv
+        dctjhkydytcyghkjluydtcxgjnfrzx\gb_njm.lkjhgchnjmjnhbyfWSRFRDfghnjgfcv123456788765rfghygtffrv
+        ),
+      course_module_id: 2
+    )
+  end
+
+  it 'is invalid if the length of the title is less than 8 characters' do
+    subject.title = 'zofgtry'
+    expect(subject).to_not be_valid
+  end
+  it 'is invalid if the length of the title is more than 40 characters' do
+    subject.title = 'dctjhkydytcyghkjluydtcxgjnfrzx$gb_njm.lkjhgchnjmjnhbyfWSRFRDfghnjgfcv123456788765rfghygtffrv'
+    expect(subject).to_not be_valid
+  end
+  it 'is invalid if the length of the content is less than 30 characters' do
+    subject.content = 'zoe1234'
+    expect(subject).to_not be_valid
+  end
+  it 'is invalid if the length of the content is more than 300 characters' do
+    subject.content = %q(
+      dctjhkydytcyghkjluydtcxgjnfrzx\gb_njm.lkjhgchnjmjnhbyfWSRFRDfghnjgfcv123456788765rfghygtffrv
+      dctjhkydytcyghkjluydtcxgjnfrzx\gb_njm.lkjhgchnjmjnhbyfWSRFRDfghnjgfcv123456788765rfghygtffrv
+      dctjhkydytcyghkjluydtcxgjnfrzx\gb_njm.lkjhgchnjmjnhbyfWSRFRDfghnjgfcv123456788765rfghygtffrv
+      dctjhkydytcyghkjluydtcxgjnfrzx\gb_njm.lkjhgchnjmjnhbyfWSRFRDfghnjgfcv123456788765rfghygtffrv
+      )
+    expect(subject).to_not be_valid
+  end
+  it 'is invalid if course_module_id is nil' do
+    subject.course_module_id = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is invalid if course_module_id is less than 1' do
+    subject.course_module_id = 0
+    expect(subject).to_not be_valid
+  end
+
+  it 'is invalid if course_module_id is not an integer' do
+    subject.course_module_id = 0
+    expect(subject).to_not be_valid
+  end
+
+
+
+
 end
