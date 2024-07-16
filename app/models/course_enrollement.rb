7609994c58bbf370282dd0193b8c1a8fb01775da
David Lafontant <lafontant.david@gmail.com>
2024-07-16 04:18:48 -0400

    Add validations for course_enrollement

--- app/models/course_enrollement.rb
+++ app/models/course_enrollement.rb
@@ -1,4 +1,8 @@
 class CourseEnrollement < ApplicationRecord
   belongs_to :student, class_name: 'User'
   belongs_to :course
+
+  validates :student_id, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :course_id, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+
 end
