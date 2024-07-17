3b48163154234944854a1914e6452edeaaf8371a
David Lafontant <lafontant.david@gmail.com>
2024-07-17 06:41:44 -0400

    Handle resources accesibility in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "Non disponible."
+    redirect_to root_url
+  end
 end
